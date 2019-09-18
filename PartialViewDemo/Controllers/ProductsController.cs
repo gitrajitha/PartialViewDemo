fdabb22556b068af37aba25cc0e0c01792ffebd8
Rajitha <Rajitha@DESKTOP-LNJ11UJ>
2019-09-18 09:23:17 +0530

    changes done

---
+++ PartialViewDemo/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartialViewDemo.Models;
+
+
+namespace PartialViewDemo.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        List<Product> products = new List<Product>()
+        {
+            new Product {ProductID=1, Name ="charger", Price=500.00, Photo = "~/images/1.jpg" },
+            new Product {ProductID=2, Name ="hub", Price=150.00, Photo = "~/images/2.jpg" },
+            new Product {ProductID=3, Name ="hdmi", Price=50.00, Photo = "~/images/3.jpg" },
+            new Product {ProductID=4, Name ="sd", Price=2500.00, Photo = "~/images/4.jpg" }
+
+
+        };
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PartialViewDemo/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartialViewDemo.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Photo { get; set; }
+
+    }
+}
